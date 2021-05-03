2f30e424030fa35561f6319747f741a199487526
WEE99 <100086343@students.swinburne.edu.my>
2021-05-03 10:10:34 +0800

    update login page

--- WC SL app/final/LoginPage.js
+++ WC SL app/final/LoginPage.js
@@ -1,102 +1,102 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState, Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity, ImageBackground, Image } from 'react-native';
-import { auth, db, storage } from "./components/firebase";
+import React, {useEffect, useState, Component} from 'react';
+import { StyleSheet, Text, View, TextInput, Button ,TouchableOpacity, ImageBackground,Image} from 'react-native';
+import {auth, db, storage} from "./components/firebase";
 
 //export default function App() {
-export default ({ navigation, route }) => {
+export default ({navigation, route}) => {
 
-  const pressLogin = () => {
-    var em = LoginEmail.charAt(LoginEmail.length - 1);
-    if (em === " ") {
-      setLoginEmail(LoginEmail.slice(0, -1));
+  const pressLogin =()=>{
+    var em = LoginEmail.charAt(LoginEmail.length-1);
+    if(em === " "){
+        setLoginEmail(LoginEmail.slice(0, -1));
     }
-    auth.signInWithEmailAndPassword(LoginEmail, Loginpassword)
-      .then((user) => {
-        console.log("LoginPages user.user.uid: " + user.user.uid);
-        alert("Login Sucessful")
-      })
-      .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode, errorMessage);
-        if (errorCode === "auth/user-not-found") {
-          alert("No Account with that Email Address");
-        } else if (errorCode === "auth/wrong-password" || errorCode === "auth/invalid-email") {
-          alert("Incorrect Email or Password");
-        } else {
-          alert("An error occurred. Please Try Again");
-        }
-      });
-  };
+        auth.signInWithEmailAndPassword(LoginEmail, Loginpassword)
+            .then((user) => {
+                console.log("LoginPages user.user.uid: "+user.user.uid);
+                alert("Login Sucessful")
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorCode,errorMessage);
+                if(errorCode === "auth/user-not-found"){
+                    alert("No Account with that Email Address");
+                }else if(errorCode === "auth/wrong-password" || errorCode === "auth/invalid-email"){
+                    alert("Incorrect Email or Password");
+                }else{
+                    alert("An error occurred. Please Try Again");
+                }
+            });
+};
 
-  const pressForgotpassword = () => {
-    alert("nav to ForgotPassword .js");
+  const pressForgotpassword =()=>{
+    // alert("nav to ForgotPassword .js");
     navigation.navigate("Forgot");
   };
 
-  const [LoginEmail, setLoginEmail] = useState("");
-  const [Loginpassword, setLoginpassword] = useState("");
+  const [LoginEmail, setLoginEmail]=useState("");
+  const [Loginpassword, setLoginpassword]=useState("");
 
   return (
     <View style={styles.container}>
-      {/* https://wallpaperaccess.com/full/1503970.jpg */}
-      {/* <ImageBackground source={{uri: ''}} style={styles.backgroundimage}> */}
-      <ImageBackground source={{ uri: 'https://images.unsplash.com/photo-1562710057-b3a85fa9ba85?ixlib=rb-1.2.1&w=1000&q=80' }} style={styles.backgroundimage}>
-        <View style={styles.LogintitleContainer}>
-          <Text style={styles.title}>Sales Customer Management System</Text>
-        </View>
+    {/* https://wallpaperaccess.com/full/1503970.jpg */}
+    {/* <ImageBackground source={{uri: ''}} style={styles.backgroundimage}> */}
+    <ImageBackground source={{uri: 'https://images.unsplash.com/photo-1562710057-b3a85fa9ba85?ixlib=rb-1.2.1&w=1000&q=80'}} style={styles.backgroundimage}>
+    <View style={styles.LogintitleContainer}>
+      <Text style={styles.title}>Sales Customer Management System</Text>
+    </View>
 
-        <View style={styles.extra}>
-          <View style={styles.usernameandpasswordcontainer}>
+    <View style={styles.extra}>
+    <View style={styles.usernameandpasswordcontainer}>
 
-            <View style={styles.usernameC}>
-              <Text style={styles.username}>Email : </Text>
-              <TextInput
-                style={styles.input}
-                placeholder='Email'
-                onChangeText={(val) => setLoginEmail(val)}
-              />
-            </View>
+    <View style={styles.usernameC}>
+      <Text style={styles.username}>Email : </Text>
+      <TextInput 
+      style={styles.input}
+      placeholder='Email'
+      onChangeText={(val) => setLoginEmail(val)}
+       />
+    </View>
 
-            <View style={styles.passwordC}>
-              <Text style={styles.password}>Password : </Text>
-              <TextInput
-                secureTextEntry={true}
-                style={styles.input}
-                placeholder='Password'
-                onChangeText={(val) => setLoginpassword(val)}
-              />
-            </View>
+    <View style={styles.passwordC}>
+      <Text style={styles.password}>Password : </Text>
+      <TextInput 
+      secureTextEntry={true} 
+      style={styles.input}
+      placeholder='Password'
+      onChangeText={(val) => setLoginpassword(val)}
+      />
+    </View>
 
-            <View>
-              <TouchableOpacity
-                onPress={pressForgotpassword}>
-                <Text style={styles.forgot}>Forgot Password?</Text>
-              </TouchableOpacity>
-            </View>
+    <View>
+     <TouchableOpacity
+      onPress= {pressForgotpassword}>
+      <Text style={styles.forgot}>Forgot Password?</Text>
+     </TouchableOpacity>
+    </View>
 
-            <View>
-              <TouchableOpacity
-                style={styles.SignINButton}
-                onPress={pressLogin}
-              //onPress={() => this._onPressLoginButton()}
-              //disabled={!this.state.isFormValid}
-              >
-                <Text style={styles.SignIN}>SIGN IN</Text>
-              </TouchableOpacity>
-            </View>
+    <View>
+        <TouchableOpacity
+         style={styles.SignINButton}
+         onPress= {pressLogin}
+         //onPress={() => this._onPressLoginButton()}
+         //disabled={!this.state.isFormValid}
+         >
+         <Text style={styles.SignIN}>SIGN IN</Text>
+     </TouchableOpacity>
+     </View>
 
-            {/* <Text >{"username====>"+ LoginEmail}</Text>
+     {/* <Text >{"username====>"+ LoginEmail}</Text>
      <Text >{"username====>"+ Loginpassword}</Text> */}
 
-            {/* <Text >{"password====>"+Base64.encode(this.state.LOGIN_password) }</Text> */}
-          </View>
-        </View>
+    {/* <Text >{"password====>"+Base64.encode(this.state.LOGIN_password) }</Text> */}
+     </View>
+     </View>
 
 
 
-        <StatusBar style="auto" />
+      <StatusBar style="auto" />
       </ImageBackground>
     </View>
   );
@@ -113,40 +113,40 @@ const styles = StyleSheet.create({
 
   backgroundimage: {
     flex: 1,
-    width: '100%',
-    height: '100%',
+    width:'100%',
+    height:'100%',
     resizeMode: "cover",
     justifyContent: "center"
 
   },
 
-  extra: {
-    paddingLeft: "10%",
-    paddingRight: "10%",
-    alignItems: 'center',
+  extra:{
+    paddingLeft:"10%",
+    paddingRight:"10%",
+    alignItems:'center',
     // marginBottom:20,
   },
 
-  usernameandpasswordcontainer: {
-    backgroundColor: "#FFFFFF",
-    padding: "10%",
-    borderWidth: 2,
-    backgroundColor: 'rgba(255, 255, 255, 0.6)',
-    borderColor: "black",
-    alignItems: 'center',
-    borderRadius: 10
+  usernameandpasswordcontainer:{
+  backgroundColor:"#FFFFFF",
+  padding: "10%",
+  borderWidth:2,
+  backgroundColor: 'rgba(255, 255, 255, 0.6)',
+  borderColor:"black",
+  alignItems:'center',
+  borderRadius:10
   },
 
   LogintitleContainer: {
-    padding: 10,
-    marginBottom: 50,
+  padding:10,
+  marginBottom:50,
   },
 
   title: {
     fontSize: 30,
     textAlign: 'center',
     fontWeight: "bold",
-    color: "#F8C018",
+    color:"#F8C018",
 
   },
 
@@ -156,22 +156,22 @@ const styles = StyleSheet.create({
     borderTopColor: '#fff',
     borderLeftColor: '#fff',
     borderRightColor: '#fff',
-    borderRadius: 10,
+    borderRadius:10,
     padding: 10,
-    marginBottom: 10,
-    marginTop: 5,
-    width: 200,
+    marginBottom:10,
+    marginTop:5,
+    width:200,
     alignSelf: 'flex-start',
   },
 
-  forgot: {
-    color: "#000000",
-    fontSize: 11,
-    textAlign: "center",
+  forgot:{
+    color:"#000000",
+    fontSize:11,
+    textAlign:"center",
   },
 
   SignINButton: {
-    width: 160,
+    width:160 ,
     marginTop: 20,
     backgroundColor: "black",
     padding: 10,
@@ -179,17 +179,17 @@ const styles = StyleSheet.create({
   },
 
   username: {
-    fontWeight: "bold"
+  fontWeight: "bold"
   },
 
-  password: {
-    fontWeight: "bold"
+  password:{
+  fontWeight: "bold"
   },
 
   SignIN: {
     color: "#F8C018",
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight:'bold',
     justifyContent: "center",
     textAlign: "center",
   },
